56aab1e3f8e2128b43d45598b4c255ce37713e8e
Oleg Samoylov <pioneeru@gmail.com>
2023-11-23 23:19:01 +0100

    nfs fix
    Signed-off-by: Oleg Samoylov <pioneeru@gmail.com>

--- v259_0-1/1-nfs.sh
+++ v259_0-1/1-nfs.sh
@@ -11,6 +11,7 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm repo update
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+   -n default \
    --set nfs.server=${NFS_SERVER_IP} \
    --set nfs.path=${NFS_SERVER_PATH}
 

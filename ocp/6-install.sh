2b8f1576bdd5f5c237a7114237a5b38b7196af77
Oleg Samoylov <pioneeru@gmail.com>
2024-07-07 15:08:36 +0200

    postgres fix

--- ocp/6-install.sh
+++ ocp/6-install.sh
@@ -99,7 +99,7 @@ ${KUBECTL} create secret docker-registry instana-registry --namespace=instana-po
   --docker-password=$DOWNLOAD_KEY
 
 helm install cnpg cloudnative-pg-0.21.1.tgz \
-  --set image.repository=artifact-public.instana.io/self-hosted-images/3rd-party/cloudnative-pg-operator \
+  --set image.repository=artifact-public.instana.io/self-hosted-images/3rd-party/operator/cloudnative-pg \
   --set image.tag=1.21.1_v0.5.0 \
   --set imagePullSecrets[0].name=instana-registry \
   --set containerSecurityContext.runAsUser=`${KUBECTL} get namespace instana-postgres -o jsonpath='{.metadata.annotations.openshift\.io\/sa\.scc\.uid-range}' | cut -d/ -f 1` \

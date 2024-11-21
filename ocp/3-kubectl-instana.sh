2bdaf5785790fca6fefa50f6b41edaa5053b1e69
Oleg Samoylov <pioneeru@gmail.com>
2024-11-22 00:26:08 +0100

    kubectl update

--- ocp/3-kubectl-instana.sh
+++ ocp/3-kubectl-instana.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
 echo "Reading credentials.env"
+source ../artefacts.env
 source ../credentials.env
 
 # Kubectl plugin
 
-cat << EOF > /etc/yum.repos.d/Instana-Product.repo
-[instana-product]
-name=Instana-Product
-baseurl=https://_:$DOWNLOAD_KEY@artifact-public.instana.io/artifactory/rel-rpm-public-virtual/
-enabled=1
-gpgcheck=0
-gpgkey=https://_:$DOWNLOAD_KEY@artifact-public.instana.io/artifactory/api/security/keypair/public/repositories/rel-rpm-public-virtual
-repo_gpgcheck=1
-EOF
+## yum
+# cat << EOF > /etc/yum.repos.d/Instana-Product.repo
+# [instana-product]
+# name=Instana-Product
+# baseurl=https://_:$DOWNLOAD_KEY@artifact-public.instana.io/artifactory/rel-rpm-public-virtual/
+# enabled=1
+# gpgcheck=0
+# gpgkey=https://_:$DOWNLOAD_KEY@artifact-public.instana.io/artifactory/api/security/keypair/public/repositories/rel-rpm-public-virtual
+# repo_gpgcheck=1
+# EOF
 
 ## check current version:
 ## https://artifact-public.instana.io/artifactory/rel-rpm-public-virtual/
 
 
-yum makecache -y
-yum install -y 'dnf-command(versionlock)'
-# yum --showduplicates list instana-kubectl | expand
+# yum makecache -y
+# yum install -y 'dnf-command(versionlock)'
+# # yum --showduplicates list instana-kubectl-plugin | expand
 
-yum versionlock delete instana-kubectl
-yum install -y instana-kubectl-275_3-1.x86_64
-yum versionlock add instana-kubectl
+# yum versionlock delete instana-kubectl-plugin
+# yum versionlock delete instana-kubectl
+# yum remove -y instana-kubectl
+# yum install -y ${KUBECTL_INSTANA_PLUGIN}
+# yum versionlock add instana-kubectl-plugin
+
+
+## wget
+## check current version:
+## https://artifact-public.instana.io/artifactory/rel-generic-instana-virtual/infrastructure/kubectl/instana-kubectl-plugin-linux_amd64-1.1.0.tar.gz
+
+wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
+tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
 
 kubectl instana --version

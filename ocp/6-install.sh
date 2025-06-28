2348b2d3c3b197fdb5c4deb78885d9a073e05686
Oleg Samoylov <pioneeru@gmail.com>
2025-06-28 14:21:21 +0200

    gateway update

--- ocp/6-install.sh
+++ ocp/6-install.sh
@@ -524,8 +524,8 @@ ${KUBECTL} wait -n instana-units --for=jsonpath='{.status.componentsStatus}'=Rea
 
 
 echo "Creating routes..."
-${KUBECTL} create route passthrough ui-client-tenant --hostname=${INSTANA_TENANT_DOMAIN} --service=gateway --port=https -n instana-core
-${KUBECTL} create route passthrough ui-client-ssl --hostname=${INSTANA_BASE_DOMAIN} --service=gateway --port=https -n instana-core
+${KUBECTL} create route passthrough ui-client-tenant --hostname=${INSTANA_TENANT_DOMAIN} --service=gateway-v2 --port=https -n instana-core
+${KUBECTL} create route passthrough ui-client-ssl --hostname=${INSTANA_BASE_DOMAIN} --service=gateway-v2 --port=https -n instana-core
 ${KUBECTL} create route passthrough acceptor  --hostname=${INSTANA_AGENT_ACCEPTOR}  --service=acceptor  --port=8600  -n instana-core
 # ${KUBECTL} create route passthrough otlp-http-acceptor --hostname=${INSTANA_OTLP_HTTP_ACCEPTOR} --service=gateway  --port=https -n instana-core
 # ${KUBECTL} create route passthrough otlp-grpc-acceptor --hostname=${INSTANA_OTLP_GRPC_ACCEPTOR} --service=gateway  --port=https -n instana-core

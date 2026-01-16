b60b6b202a604dc959977537b9da339be729dd81
Oleg Samoylov <pioneeru@gmail.com>
2026-01-16 19:48:41 +0100

    routes

--- ocp/6-install.sh
+++ ocp/6-install.sh
@@ -536,6 +536,6 @@ echo "Creating routes..."
 ${KUBECTL} create route passthrough ui-client-tenant --hostname=${INSTANA_TENANT_DOMAIN} --service=gateway-v2 --port=https -n instana-core
 ${KUBECTL} create route passthrough ui-client-ssl --hostname=${INSTANA_BASE_DOMAIN} --service=gateway-v2 --port=https -n instana-core
 ${KUBECTL} create route passthrough acceptor  --hostname=${INSTANA_AGENT_ACCEPTOR}  --service=acceptor  --port=8600  -n instana-core
-# ${KUBECTL} create route passthrough otlp-http-acceptor --hostname=${INSTANA_OTLP_HTTP_ACCEPTOR} --service=gateway  --port=https -n instana-core
-# ${KUBECTL} create route passthrough otlp-grpc-acceptor --hostname=${INSTANA_OTLP_GRPC_ACCEPTOR} --service=gateway  --port=https -n instana-core
+${KUBECTL} create route passthrough otlp-http-acceptor --hostname=${INSTANA_OTLP_HTTP_ACCEPTOR} --service=gateway-v2  --port=https -n instana-core
+${KUBECTL} create route passthrough otlp-grpc-acceptor --hostname=${INSTANA_OTLP_GRPC_ACCEPTOR} --service=gateway-v2  --port=https -n instana-core
 echo "Done."

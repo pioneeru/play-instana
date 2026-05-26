13fac100a13fac3722c72b55a132bff419b83e5f
Oleg Samoylov <pioneeru@gmail.com>
2026-05-26 10:10:15 +0200

    fixed installing prerequisites

--- ocp/3-install-prerequisites_on_cluster.sh
+++ ocp/3-install-prerequisites_on_cluster.sh
@@ -7,7 +7,7 @@ source ../artifacts-${INSTANA_PLATFORM}.env
 # Install NFS client - if needed
 if [[ -z "${NFS_SERVER_IP}" && -z "${NFS_SERVER_PATH}" ]]; then
    echo "Installing or upgrading nfs-subdir-external-provisioner-${NFS_CLIENT_HELM_CHART_VERSION}..."
-   helm upgrade --install nfs-subdir-external-provisioner nfs-subdir-external-provisioner-${NFS_CLIENT_HELM_CHART_VERSION}.tgz \
+   helm upgrade --install nfs-subdir-external-provisioner ${INSTANA_AIRGAPPED_FOLDER}/nfs-subdir-external-provisioner-${NFS_CLIENT_HELM_CHART_VERSION}.tgz \
       -n default \
       --set nfs.server=${NFS_SERVER_IP} \
       --set nfs.path=${NFS_SERVER_PATH}
@@ -17,7 +17,7 @@ fi
 
 # Install cert manager
 echo "Installing or upgrading cert-manager-v${CERT_MANAGER_HELM_CHART_VERSION}..."
-helm upgrade --install cert-manager cert-manager-v${CERT_MANAGER_HELM_CHART_VERSION}.tgz \
+helm upgrade --install cert-manager ${INSTANA_AIRGAPPED_FOLDER}/cert-manager-v${CERT_MANAGER_HELM_CHART_VERSION}.tgz \
   --create-namespace --namespace=cert-manager \
   --set crds.enabled=true \
   --set prometheus.enabled=false \
